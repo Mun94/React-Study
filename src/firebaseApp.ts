9d90a176705062d4538d1ec5e7ac97154e201386
Mun Seong Woo <seongwoo720@gmail.com>
2023-11-09 20:55:59 +0900

    유저 인증 ì(로그인 ë¡, 로그아웃, 회원가입) 설정

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,7 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import "firebase/auth";
+import { getFirestore } from 'firebase/firestore'
+
+export let app: FirebaseApp;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,11 +10,19 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
 
-export default firebaseApp
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+
+export default firebase;
