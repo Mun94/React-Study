80a2fe9d2d7d5ec6646fd42fdb3543533f332ad7
Mun Seong Woo <seongwoo720@gmail.com>
2024-04-15 09:38:42 +0900

    ing

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+
+import Sidebar from './sidebar'
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <>
+      <Sidebar />
+    </>
   );
 }
 
